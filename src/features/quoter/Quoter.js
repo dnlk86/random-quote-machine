fb26e50065713271cca078741399cd0498f01794
Daniel Keicher <dnlkch86@gmail.com>
2022-11-09 10:40:32 +0100

    removed few unused elements

--- src/features/quoter/Quoter.js
+++ src/features/quoter/Quoter.js
@@ -15,36 +15,34 @@ export function Quoter() {
     const dispatch = useDispatch();
 
     return (
-        <div style={cardStyle}>
-            <div id="quote-box" className={styles.row}>
-                <div>
-                    <span id="author" className={styles.value}>
-                        {quote.author}:{" "}
-                    </span>
-                    <span id="text" className={styles.value}>
-                        "{quote.text}"
-                    </span>
-                </div>
-                <div>
-                    <a
-                        id="tweet-quote"
-                        href="https://twitter.com/intent/tweet"
-                        target={"_blank"}
-                        rel="noreferrer"
-                    >
-                        Tweet this quote!
-                    </a>
-                </div>
-                <div>
-                    <button
-                        id="new-quote"
-                        className={styles.button}
-                        aria-label="Generate a new random quote"
-                        onClick={() => dispatch(generate())}
-                    >
-                        Click here!
-                    </button>
-                </div>
+        <div id="quote-box" className={styles.row} style={cardStyle}>
+            <div>
+                <span id="author" className={styles.value}>
+                    {quote.author}:{" "}
+                </span>
+                <span id="text" className={styles.value}>
+                    "{quote.text}"
+                </span>
+            </div>
+            <div>
+                <a
+                    id="tweet-quote"
+                    href="https://twitter.com/intent/tweet"
+                    target={"_blank"}
+                    rel="noreferrer"
+                >
+                    Tweet this quote!
+                </a>
+            </div>
+            <div>
+                <button
+                    id="new-quote"
+                    className={styles.button}
+                    aria-label="Generate a new random quote"
+                    onClick={() => dispatch(generate())}
+                >
+                    Click here!
+                </button>
             </div>
         </div>
     );
